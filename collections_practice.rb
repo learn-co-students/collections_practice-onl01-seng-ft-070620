fbeecb8ac18f42a7e0d8885e951dac82485e10ca
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-08 15:51:06 -0700

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
 
+def sort_array_asc(array)
+    array.sort
+end 
+
+def sort_array_desc(array)
+    array.sort {|a,b| b <=> a}
+end 
+
+def sort_array_char_count(array)
+    array.sort {|a, b| a.length <=> b.length}
+end 
+
+def swap_elements(array)
+    array.sort {|a, b| a[2] <=> b[1]}
+end 
+
+def reverse_array(array)
+    array.reverse 
+end 
+
+def kesha_maker(array)
+    kesha = []
+    array.each do |word|
+        word[2] = "$"
+        kesha << word
+    end 
+    kesha 
+end
+
+def find_a(array)
+    array.select {|word| word[0] == "a"}
+end 
+
+def sum_array(array)
+    sum = 0
+    array.each {|num| sum += num}
+    sum
+end 
+
+def add_s(array)
+    array.each_with_index.collect do |element, index|
+        if index != 1
+            element.insert(-1, "s")
+        else 
+            element
+        end 
+    end
+end 
